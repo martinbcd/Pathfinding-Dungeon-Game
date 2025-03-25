ab9348e30e4612bdf2263af81ece283a2be0e5c1
martinbcd <57986363+martinbcd@users.noreply.github.com>
2025-03-25 15:01:56 +0100

    added basic program structure

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 import java.util.*;
 
 public class Main {
+
+    static class Cell {
+        int row, col, health;
+
+        Cell(int row, int col, int health) {
+            this.row = row;
+            this.col = col;
+            this.health = health;
+        }
+    }
+
+    public int dijkstras(int[][] dungeon) {
+        PriorityQueue<Cell> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a.health));
+        int m = dungeon.length, n = dungeon[0].length;
+        int[][] minHealth = new int[m][n];
+        return 0;
+    }
+
     public static void main(String[] args) {
+
     }
 }
